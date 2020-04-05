1ecb665e1e24b97306e78a9b714d275c1c8bd1f6
Tomachi (ICHIGO) <lego@aol.jp>
2020-04-05 16:25:23 +0900

    add: No.2「はじめてのもどかしさ」の実績追加

---
+++ src/main/java/com/jaoafa/jaoSuperAchievement2/Achievements/FirstImpatience.java
@@ -0,0 +1,39 @@
+package com.jaoafa.jaoSuperAchievement2.Achievements;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+
+import com.jaoafa.jaoSuperAchievement2.API.AchievementAPI;
+import com.jaoafa.jaoSuperAchievement2.API.Achievementjao;
+import com.jaoafa.jaoSuperAchievement2.Lib.AchievementType;
+
+/**
+ * No. 2
+ *
+ * はじめてのもどかしさ
+ * 「.」と発言する
+ * 初めて「.」と発言した場合
+ *
+ * @since 2020/04/05
+ * @category jao Achievement
+ *
+ */
+@SuppressWarnings("deprecation")
+public class FirstImpatience implements Listener {
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void OnSpeakjaotan(PlayerChatEvent event) {
+		String message = event.getMessage();
+		Player player = event.getPlayer();
+		if (!message.equalsIgnoreCase(".")) {
+			return;
+		}
+
+		if (!Achievementjao.getAchievement(player, new AchievementType(2))) {
+			player.sendMessage(AchievementAPI.getPrefix() + "実績の解除中に問題が発生しました。もう一度お試しください。");
+			return;
+		}
+	}
+}
