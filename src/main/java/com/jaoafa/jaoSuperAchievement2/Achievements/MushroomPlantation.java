bcb580c63feb2aae954d3f1790e316d5243a645c
Tomachi (ICHIGO) <lego@aol.jp>
2020-04-17 00:40:54 +0900

    add: No.29「きのこ栽培工場」の実績追加

--- src/main/java/com/jaoafa/jaoSuperAchievement2/Achievements/MushroomPlantation.java
+++ src/main/java/com/jaoafa/jaoSuperAchievement2/Achievements/MushroomPlantation.java
@@ -14,25 +14,25 @@ import com.jaoafa.jaoSuperAchievement2.API.Achievementjao;
 import com.jaoafa.jaoSuperAchievement2.Lib.AchievementType;
 
 /**
- * No. 28
+ * No. 29
  *
- * 熊出没注意
- * mine_book000と時間を共にする
- * mine_book000がいる時にログインする
+ * きのこ栽培工場
+ * Hirotaisou2012と時間を共にする
+ * Hirotaisou2012がいる時にログインする
  *
  * @since 2020/04/17
  * @category jao Achievement
  *
  */
-public class BewareOfBears implements Listener {
+public class MushroomPlantation implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void OnFirst_mine_book000(PlayerJoinEvent event) {
+	public void OnFirstHirotaisou2012(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 
-		UUID mine_book000_uuid = UUID.fromString("32ff7cdc-a1b4-450a-aa7e-6af75fe8c37c");
+		UUID mine_book000_uuid = UUID.fromString("39cf878b-ef0b-44fc-a2c6-de3d540a4728");
 		if (player.getUniqueId().equals(mine_book000_uuid)) {
 			for (Player play : Bukkit.getServer().getOnlinePlayers()) {
-				if (!Achievementjao.getAchievement(play, new AchievementType(28))) {
+				if (!Achievementjao.getAchievement(play, new AchievementType(29))) {
 					play.sendMessage(AchievementAPI.getPrefix() + "実績の解除中に問題が発生しました。もう一度お試しください。");
 					return;
 				}
@@ -47,7 +47,7 @@ public class BewareOfBears implements Listener {
 			return;
 
 		// どうせgetAchievement側ですでに取得してるかどうかは検査されるのでそのまま。
-		if (!Achievementjao.getAchievement(player, new AchievementType(28))) {
+		if (!Achievementjao.getAchievement(player, new AchievementType(29))) {
 			player.sendMessage(AchievementAPI.getPrefix() + "実績の解除中に問題が発生しました。もう一度お試しください。");
 			return;
 		}
