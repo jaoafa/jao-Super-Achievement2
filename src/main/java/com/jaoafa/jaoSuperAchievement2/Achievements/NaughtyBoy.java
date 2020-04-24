5403f9d37af6ec8d830fde929ba6ea605d582e80
Tomachi (ICHIGO) <lego@aol.jp>
2020-04-24 10:13:42 +0900

    add: No.37「わんぱく少年」の実績追加

---
+++ src/main/java/com/jaoafa/jaoSuperAchievement2/Achievements/NaughtyBoy.java
@@ -0,0 +1,45 @@
+package com.jaoafa.jaoSuperAchievement2.Achievements;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.ItemStack;
+
+import com.jaoafa.jaoSuperAchievement2.API.AchievementAPI;
+import com.jaoafa.jaoSuperAchievement2.API.Achievementjao;
+import com.jaoafa.jaoSuperAchievement2.Lib.AchievementType;
+
+/**
+ * No. 37
+ *
+ * わんぱく少年
+ * 両手に雪玉をもつ
+ * オフハンドとメインハンドに雪玉を持つ
+ *
+ * @since 2020/04/24
+ * @category jao Achievement
+ *
+ */
+public class NaughtyBoy implements Listener {
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void OnNaughtyBoy(InventoryCloseEvent event) { // インベントリを閉じたとき
+		if (!(event.getPlayer() instanceof Player)) {
+			return;
+		}
+		Player player = (Player) event.getPlayer();
+
+		ItemStack main = player.getInventory().getItemInMainHand();
+		ItemStack off = player.getInventory().getItemInOffHand();
+		if (main.getType() != Material.SNOW_BALL || off.getType() != Material.SNOW_BALL) {
+			return;
+		}
+
+		if (!Achievementjao.getAchievement(player, new AchievementType(37))) {
+			player.sendMessage(AchievementAPI.getPrefix() + "実績の解除中に問題が発生しました。もう一度お試しください。");
+			return;
+		}
+	}
+}
