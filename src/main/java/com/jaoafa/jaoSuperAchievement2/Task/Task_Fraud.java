fe71e4fe1964abcdfda7fefeebda50ec2c349dbe
Tomachi (ICHIGO) <lego@aol.jp>
2020-04-24 09:36:40 +0900

    add: No.34「詐欺」の実績追加

---
+++ src/main/java/com/jaoafa/jaoSuperAchievement2/Task/Task_Fraud.java
@@ -0,0 +1,24 @@
+package com.jaoafa.jaoSuperAchievement2.Task;
+
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.jaoafa.jaoSuperAchievement2.API.AchievementAPI;
+import com.jaoafa.jaoSuperAchievement2.API.Achievementjao;
+import com.jaoafa.jaoSuperAchievement2.Lib.AchievementType;
+
+public class Task_Fraud extends BukkitRunnable {
+	Player player;
+
+	public Task_Fraud(Player player) {
+		this.player = player;
+	}
+
+	@Override
+	public void run() {
+		if (!Achievementjao.getAchievement(player, new AchievementType(34))) {
+			player.sendMessage(AchievementAPI.getPrefix() + "実績の解除中に問題が発生しました。もう一度お試しください。");
+			return;
+		}
+	}
+}
