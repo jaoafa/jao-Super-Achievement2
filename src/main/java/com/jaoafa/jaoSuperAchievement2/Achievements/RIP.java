724a636697af028f32b20c92f73a2ee7862c022c
Tomachi (ICHIGO) <lego@aol.jp>
2020-04-24 10:16:46 +0900

    add: No.38「R.I.P.」の実績追加

---
+++ src/main/java/com/jaoafa/jaoSuperAchievement2/Achievements/RIP.java
@@ -0,0 +1,33 @@
+package com.jaoafa.jaoSuperAchievement2.Achievements;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import com.jaoafa.jaoSuperAchievement2.API.AchievementAPI;
+import com.jaoafa.jaoSuperAchievement2.API.Achievementjao;
+import com.jaoafa.jaoSuperAchievement2.Lib.AchievementType;
+
+/**
+ * No. 38
+ *
+ * R.I.P.
+ * 安らかに眠れ
+ * いずれかの方法で死亡する
+ *
+ * @since 2020/04/24
+ * @category jao Achievement
+ *
+ */
+public class RIP implements Listener {
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onDeath(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+		if (!Achievementjao.getAchievement(player, new AchievementType(38))) {
+			player.sendMessage(AchievementAPI.getPrefix() + "実績の解除中に問題が発生しました。もう一度お試しください。");
+			return;
+		}
+	}
+}
