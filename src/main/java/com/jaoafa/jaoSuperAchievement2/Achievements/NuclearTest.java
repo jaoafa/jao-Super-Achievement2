6e3db84ffcccb2a91cc9492117c39c9842121d05
Tomachi [ICHIGO] <lego@aol.jp>
2021-03-27 18:08:28 +0900

    add: No.79の実績追加

---
+++ src/main/java/com/jaoafa/jaoSuperAchievement2/Achievements/NuclearTest.java
@@ -0,0 +1,43 @@
+package com.jaoafa.jaoSuperAchievement2.Achievements;
+
+import com.jaoafa.jaoSuperAchievement2.API.AchievementAPI;
+import com.jaoafa.jaoSuperAchievement2.API.Achievementjao;
+import com.jaoafa.jaoSuperAchievement2.Lib.AchievementType;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+
+/**
+ * No. 79
+ *
+ * 核実験
+ * TNTを所持する
+ * TNTを所持した場合
+ * ※隠し要素
+ *
+ * @category jao Achievement
+ * @since 2021/03/27
+ */
+public class NuclearTest implements Listener {
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void OnGetTNT(InventoryCloseEvent event) { // インベントリを閉じたとき
+        if (!(event.getPlayer() instanceof Player)) {
+            return;
+        }
+        Player player = (Player) event.getPlayer();
+
+        if(Arrays.stream(player.getInventory().getContents()).noneMatch(item -> item.getType() == Material.TNT)){
+            return;
+        }
+
+        if (!Achievementjao.getAchievement(player, new AchievementType(79))) {
+            player.sendMessage(AchievementAPI.getPrefix() + "実績の解除中に問題が発生しました。もう一度お試しください。");
+        }
+    }
+}
